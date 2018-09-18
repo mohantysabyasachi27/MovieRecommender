141bdd767666748a1183dfcda042c1a0035b0a5f
Sunny <mohantysabyasachi27@gmail.com>
2018-09-18 00:26:44 -0700

    Initial Commit for the Backend Component -- Added MongoTemplate & RestTemplate

--- src/main/java/com/asu/MovieRecommender/config/MovieRecommenderConfig.java
+++ src/main/java/com/asu/MovieRecommender/config/MovieRecommenderConfig.java
@@ -1,5 +1,29 @@
 package com.asu.MovieRecommender.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.web.client.RestTemplate;
+
+import com.mongodb.MongoClient;
+
 public class MovieRecommenderConfig {
 
+	@Bean
+	public RestTemplate restTemlate() {
+		return new RestTemplate();
+	}
+	
+	public @Bean
+	MongoDbFactory mongoDbFactory() throws Exception {
+		return new SimpleMongoDbFactory(new MongoClient(), "movie");
+	}
+
+	public @Bean
+	MongoTemplate mongoTemplate() throws Exception {
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+		return mongoTemplate;
+	}
+	
 }
---
+++ src/main/java/com/asu/MovieRecommender/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.asu.MovieRecommender.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.client.RestTemplate;
+
+public class MovieController {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	//ToDo - Write an API to get the now playing movies
+	
+	
+	
+}
