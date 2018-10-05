68773e97c51b629dbaf4eb0a460c65aef42bacaf
Sunny <mohantysabyasachi27@gmail.com>
2018-10-05 02:29:06 -0700

    #35 - Changed the cors filter for getMovies api

--- src/main/java/com/asu/MovieRecommender/Controllers/RegisterController.java
+++ src/main/java/com/asu/MovieRecommender/Controllers/RegisterController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.asu.MovieRecommender.User.Response;
 import com.asu.MovieRecommender.User.User;
 
 @RestController
+@CrossOrigin(origins="*", allowedHeaders="*")
 public class RegisterController {
 
 	@Autowired
