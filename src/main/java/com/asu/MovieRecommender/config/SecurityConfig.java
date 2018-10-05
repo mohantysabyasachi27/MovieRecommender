03003cb89360e4c9ce772982c96fedbcc5c9feab
Sunny <mohantysabyasachi27@gmail.com>
2018-10-05 02:15:33 -0700

    #35 - Changed the cors filter for getMovies api

--- src/main/java/com/asu/MovieRecommender/config/SecurityConfig.java
+++ src/main/java/com/asu/MovieRecommender/config/SecurityConfig.java
@@ -74,6 +74,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("*"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
