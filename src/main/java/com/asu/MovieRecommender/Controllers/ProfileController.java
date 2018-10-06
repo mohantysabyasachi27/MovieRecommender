19259d5911ff795efe3b7dca143ebf69f29a0cb9
kumarkalyan93 <kkalyan3@asu.edu>
2018-10-05 18:52:04 -0700

    Added Cross Origins for Profile Controller

--- src/main/java/com/asu/MovieRecommender/Controllers/ProfileController.java
+++ src/main/java/com/asu/MovieRecommender/Controllers/ProfileController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import com.asu.MovieRecommender.User.Response;
 import com.asu.MovieRecommender.User.User;
 
 @RestController
+@CrossOrigin(origins="*", allowedHeaders="*")
 public class ProfileController {
 
 	@Autowired
