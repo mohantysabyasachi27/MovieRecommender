2c68e07d625b9043fa27c0bdaac0c989cef1e8a5
kumarkalyan93 <kkalyan3@asu.edu>
2018-10-26 18:12:16 -0700

    UserStory-63,Task-76,EmailNotification Service added

---
+++ src/main/java/com/asu/MovieRecommender/Services/EmailNotificationService.java
@@ -0,0 +1,46 @@
+package com.asu.MovieRecommender.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.asu.MovieRecommender.User.User;
+
+@Service
+public class EmailNotificationService {
+
+	
+	@Autowired
+	private MailBuilder mailBuilder;
+	
+	private JavaMailSender mailSender;
+	 
+    @Autowired
+     public EmailNotificationService(JavaMailSender mailSender) {
+    	  this.mailSender = mailSender;
+    	  
+	}
+    
+    
+    public void sendMail(User user) throws Exception
+    {
+    	MimeMessagePreparator message = messageSender->{
+    		MimeMessageHelper helper = new MimeMessageHelper(messageSender);
+    		helper.setTo(user.getUserEmailId());
+    		helper.setFrom("kmrprabhu93@gmail.com");
+    		helper.setSubject("Registration Success");
+    		 String content = mailBuilder.build(user.getFirstName(),"Thank You for registering");
+    	        helper.setText(content, true);
+    	};
+    	
+    	try{mailSender.send(message);}
+    	catch(MailException m){
+    		throw new Exception(m.getMessage());
+    	}
+    	
+    }
+    
+}
