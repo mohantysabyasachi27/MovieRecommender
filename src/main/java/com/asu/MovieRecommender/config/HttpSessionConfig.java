303b840dc7fde07ce130fa535bc00a2497e193fe
Sunny <mohantysabyasachi27@gmail.com>
2018-11-02 16:04:35 -0700

    User Story - #91, Task#98 - Send session information to Angular Componenent

--- src/main/java/com/asu/MovieRecommender/config/HttpSessionConfig.java
+++ src/main/java/com/asu/MovieRecommender/config/HttpSessionConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
 @EnableRedisHttpSession
@@ -15,5 +17,11 @@ public class HttpSessionConfig {
         connection.setHostName("redis");
         return connection;
     }
+    
+    @Bean
+	public HttpSessionIdResolver httpSessionIdResolver() {
+		return HeaderHttpSessionIdResolver.xAuthToken(); 
+	}
+    
     
 }
