c4691d44ba4e56b0daf2ddd8719cc9fda3d52c79
kumarkalyan93 <kkalyan3@asu.edu>
2018-11-19 21:14:59 -0700

    Added model for show details

---
+++ src/main/java/com/asu/MovieRecommender/ws/themoviedb/ShowDetails.java
@@ -0,0 +1,41 @@
+package com.asu.MovieRecommender.ws.themoviedb;
+
+import java.io.Serializable;
+
+public class ShowDetails implements Serializable {
+	
+	private String cinemaId;
+	public ShowDetails(String cinemaId, String bookingLink, String showTime) {
+		super();
+		this.cinemaId = cinemaId;
+		this.bookingLink = bookingLink;
+		this.showTime = showTime;
+	}
+	
+	public ShowDetails() {
+		
+	}
+	
+	public String getCinemaId() {
+		return cinemaId;
+	}
+	public void setCinemaId(String cinemaId) {
+		this.cinemaId = cinemaId;
+	}
+	public String getBookingLink() {
+		return bookingLink;
+	}
+	public void setBookingLink(String bookingLink) {
+		this.bookingLink = bookingLink;
+	}
+	public String getShowTime() {
+		return showTime;
+	}
+	public void setShowTime(String showTime) {
+		this.showTime = showTime;
+	}
+	private String bookingLink;
+	private String showTime;
+	
+
+}
